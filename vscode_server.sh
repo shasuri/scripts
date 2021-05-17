325ba9af9fdb38e60c5b6951260d7492afd4929c
shasuri <ghimmk@gmail.com>
2021-05-17 23:55:21 +0900

    write usage for vscode_server.sh

--- vscode_server.sh
+++ vscode_server.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-powershell.exe code $1
+if (($# < 1)); then
+	echo usage : ./vscode_server yourVSCWorkspacePath
+	exit 2
+fi
+
+ls ${1}
+
+if [ $? -eq 0 ]; then
+	powershell.exe code ${1}
+else
+	echo wrong path!
+fi
 # powershell.exe code yourVSCWorkspacePath
