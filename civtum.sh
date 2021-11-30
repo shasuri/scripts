cd6b5f5f7964945dd8f5d1baf3398537036d3e04
shasuri <ghimmk@gmail.com>
2021-11-30 21:38:15 +0900

    remove unpermanent scriopts

--- civtum.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./chrome.sh /mnt/c/shasuri/web/civtum/index.html
--- send_file.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-scp -P 10022 ${1} ${2}@${3}:${4}
-# usage : scp -P 10022 yourfile yourID@serverIP:serverPath
