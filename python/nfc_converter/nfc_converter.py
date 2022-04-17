7e56210d1ebfe66d2c38a2fb563e790f6d971a60
shasuri <ghimmk@gmail.com>
2022-04-18 02:02:55 +0900

    Feat : file 기능 완성
    modified:   python/nfc_converter/nfc_converter.py

--- python/nfc_converter/nfc_converter.py
+++ python/nfc_converter/nfc_converter.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import argparse
 from typing import List
@@ -7,27 +8,49 @@ def get_converted_string(string_convert: str):
     converted_string = unicodedata.normalize('NFC',string_convert)
     return converted_string
 
-def convert_strings(string_list: List[str]):
-    converted_string_list = [get_converted_string(s) for s in string_list]
+def convert_texts(text_list: List[str]):
+    converted_string_list = [get_converted_string(t) for t in text_list]
     return converted_string_list
 
-def convert_save_file_name():
-    pass
+def convert_save_file_name(file_path_list: List[str]) -> None:
+    renamed_count = 0
+    print("Renamed : ",end='')
+    for file_path in file_path_list:
+        file_name = os.path.basename(file_path)
+        dir_name = os.path.dirname(file_path)
+
+        converted_file_name = get_converted_string(file_name)
+        new_file_path = os.path.join(dir_name,converted_file_name)
+
+        os.rename(file_path,new_file_path)
+        renamed_count += 1
+        
+        if renamed_count != 1:
+            print("\t  ",end='')
+        print(f"{file_name} -> {converted_file_name}")
+    print(f"{renamed_count} file names fixed.")
+
+def execute_script(arguments: argparse.Namespace):
+    if(arguments.file != None):
+        arguments.file(arguments.strings_convert)
+
+    if(arguments.text != None):
+        arguments.text(arguments.strings_convert)
 
 if __name__ == "__main__" :
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("file_names",metavar='F', type=str, nargs='+',
-    help="Separated file names to fix.")
+    parser.add_argument("strings_convert",metavar='F', type=str, nargs='+',
+        help="Separated file names to fix.")
 
     parser.add_argument("-f","--file", action='store_const',
-    default=convert_save_file_name, const=convert_save_file_name,
-    help="Fix the separated file names and save (default)")
+        const=convert_save_file_name,
+        help="Fix the separated file names and save (default)")
 
     parser.add_argument("-t","--text", action='store_const',
-    const=convert_strings,
-    help="Fix the separated texts and print")
+        const=convert_texts,
+        help="Fix the separated texts and print")
     
     args = parser.parse_args()
-    print(args)
-    print(args.text(args.file_names))
+    
+    execute_script(args)
