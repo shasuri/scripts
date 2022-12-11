bd19137339c6ae67e6842adfc3dd373aafbabd03
shasuri <ghimmk@gmail.com>
2022-12-11 19:02:45 +0900

    [slack_committer] feat : 사용자 맵 기능 추가, 함수 정리 및 타입 정리

--- python/slack_committer/slack_committer.py
+++ python/slack_committer/slack_committer.py
@@ -2,7 +2,12 @@ import os
 from glob import glob
 import json
 from datetime import datetime
-from typing import List, Dict
+from typing import List, Dict, Union
+
+JsonObject = Dict[str, str | int | float]
+JsonArray = List[JsonObject]
+# JsonMsg = Dict[str, str]
+
 
 PATCH_DELIMETER: str = "keeper_db"
 LOG_DIR: str = "/home/ghimmk/db_log"
@@ -24,34 +29,71 @@ class PatchNote:
         self.uploaded_files = uploaded_files
 
 
+class AnalyzedLog:
+    patch_notes: List[PatchNote]
+    user_map: Dict[str, str]
+
+    def __init__(self, patch_notes: List[PatchNote], user_map: Dict[str, str]) -> None:
+        self.patch_notes = patch_notes
+        self.user_map = user_map
+
+
+class User:
+    code: str
+    name: str
+
+    def __init__(self, code: str, name: str) -> None:
+        self.code = code
+        self.name = name
+
+
 def slack_commit():
-    log_files: List[str] = get_logs(LOG_DIR, LOG_GLOB_PATTERN)
+    log_files: List[str] = get_log_files(LOG_DIR, LOG_GLOB_PATTERN)
     analyze_log_files(log_files)
 
 
-def get_logs(log_path: str, log_pattern: str) -> List[str]:
+def get_log_files(log_path: str, log_pattern: str) -> List[str]:
     return sorted(glob(log_path + '/' + log_pattern))
 
 
 def analyze_log_files(log_files: List[str]) -> List[PatchNote]:
     patch_notes: List[PatchNote] = list()
+    user_map: Dict[str, str] = dict()
+
+    user: User
 
     for day_log_file in log_files:
         with open(day_log_file, 'r') as day_log:
-            messages: List[Dict] = json.load(day_log)
+            messages: JsonArray = json.load(day_log)
+
             for msg in messages:
-                if(is_patch_note(msg)):
-                    patch_notes.append(extract_patch_note(msg))
+
+                if is_user_profile_included(msg) and msg["user"] not in user_map:
+                    user = get_user_map(msg)
+                    user_map[user.code] = user.name
+
+                if is_patch_note(msg):
+                    patch_notes.append(get_patch_note(msg))
 
     return patch_notes
 
 
-def is_patch_note(msg: Dict[str]) -> bool:
+def is_user_profile_included(msg: JsonObject) -> bool:
+    return ("user_profile" in msg)
+
+
+def get_user_map(msg: JsonObject) -> User:
+    code = msg["user"]
+    name = msg["user_profile"]["real_name"]
+
+    return User(code, name)
+
+
+def is_patch_note(msg: JsonObject) -> bool:
     return msg["text"].startswith(PATCH_DELIMETER)
 
 
-def extract_patch_note(msg: str) -> PatchNote:
-
+def get_patch_note(msg: JsonObject) -> PatchNote:
     content: str = msg["text"]
     send_time: datetime = datetime.fromtimestamp(float(msg["ts"]))
     uploaded_files: List[str] = [f["name"] for f in msg["files"]]
