4332b4e9dd1491b8618646d20c93278f6cd5b649
shasuri <ghimmk@gmail.com>
2022-12-16 13:14:43 +0900

    [slack_committer] test : id to name 변환 함수 테스트 작성

--- python/slack_committer/slack_committer_test.py
+++ python/slack_committer/slack_committer_test.py
@@ -1,7 +1,7 @@
 import unittest
 from slack_committer import *
 import fnmatch
-from typing import List
+from pprint import pprint
 
 
 class StringPatternTester(unittest.TestCase):
@@ -22,23 +22,36 @@ class StringPatternTester(unittest.TestCase):
         self.assertTrue(is_patch_note(j1))
         self.assertTrue(is_patch_note(j2))
 
+
+class ConvertTester(unittest.TestCase):
     def test_convert_to_plaintext(self):
         s: str = "keeper_db 1.11.0\n• 20220824 DB 업데이트\n• 테이블을 추가했습니다.\n    ◦ `merit_log`\n        ▪︎ 상\/벌점 관련 로그 테이블입니다.\n        ▪︎ 부여자(관리자만 가능), 수여자, 시간, 상\/벌점 유형을 기록합니다.\n    ◦ `merit_type`\n        ▪︎ 상\/벌점 유형을 기록하는 테이블입니다.\n        ▪︎ 점수, 상벌점 여부, 세부내용을 기록합니다.\n        ▪︎ `content` : LONGTEXT -&gt; TINYTEXT"
         md: str = "keeper_db 1.11.0\n- 20220824 DB 업데이트\n- 테이블을 추가했습니다.\n    - `merit_log`\n        - 상\/벌점 관련 로그 테이블입니다.\n        - 부여자(관리자만 가능), 수여자, 시간, 상\/벌점 유형을 기록합니다.\n    - `merit_type`\n        - 상\/벌점 유형을 기록하는 테이블입니다.\n        - 점수, 상벌점 여부, 세부내용을 기록합니다.\n        - `content` : LONGTEXT -> TINYTEXT"
 
         self.assertEqual(convert_to_plaintext(s), md)
 
+    def test_convert_userid_to_username(self):
+        user_map: Dict[str, str] = {"U02RSJATULA": "gusah009",
+                                    "U02RL5VEJ0N": "lkukh17"}
+        userid_text: str = "keeper_db 0.18.0\n• 20220214 DB패치노트\n• 기본 데이터가 추가되었습니다. `category` <@U02RSJATULA> <@U02RL5VEJ0N> \n    ◦ 대조표의 기본데이터 시트에 기록해두었습니다."
+        username_text: str = "keeper_db 0.18.0\n• 20220214 DB패치노트\n• 기본 데이터가 추가되었습니다. `category` @gusah009 @lkukh17 \n    ◦ 대조표의 기본데이터 시트에 기록해두었습니다."
+
+        self.assertEqual(convert_userid_to_username(
+            userid_text, user_map), username_text)
+
 
 def print_patch_notes():
     log_files: List[str] = get_log_files(LOG_DIR, LOG_GLOB_PATTERN)
-    print(log_files)
+    # print(log_files)
     analyzed_log: AnalyzedLog = analyze_log_files(log_files)
 
-    convert_patch_notes_format(analyzed_log.patch_notes)
+    convert_patch_notes_format(analyzed_log)
     for p in analyzed_log.patch_notes:
         print(p.content)
         print(p.uploaded_files)
         print("\n===========================\n")
+
+    pprint(analyzed_log.user_map)
 
 
 if __name__ == "__main__":
